03711a51763ec3e3fb7d35a10d1d2e5bc65296bd
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2023-02-08 18:24:32 +0900

    feat(home_screen): StatefulWidget으로 변경 및 기초 구조 작성

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<StatefulWidget> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? video;
+
+  @override
   Widget build(BuildContext context) {
-    return Text('Home Screen');
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: video == null ? renderEmpty() : renderVideo(),
+    );
+  }
+
+  Widget renderEmpty() {
+    return Container();
+  }
+
+  Widget renderVideo() {
+    return Container();
   }
 }
