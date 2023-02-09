c2938c8cf3dd8a324475ea6db6538c37347adaa8
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2023-02-09 18:46:12 +0900

    feat(custom_video_player): 동영상 인식 가능하도록 작성

--- lib/component/custom_video_player.dart
+++ lib/component/custom_video_player.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
 
 class CustomVideoPlayer extends StatefulWidget {
   final XFile video;
@@ -11,12 +14,39 @@ class CustomVideoPlayer extends StatefulWidget {
 }
 
 class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  VideoPlayerController? videoController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initializeController();
+  }
+
+  initializeController() async {
+    final videoController = VideoPlayerController.file(
+      File(widget.video.path),
+    );
+
+    await videoController.initialize();
+
+    setState(() {
+      this.videoController = videoController;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'CustomVideoPlayer',
-        style: TextStyle(color: Colors.white),
+    if (videoController == null) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return AspectRatio(
+      aspectRatio: videoController!.value.aspectRatio,
+      child: VideoPlayer(
+        videoController!,
       ),
     );
   }
